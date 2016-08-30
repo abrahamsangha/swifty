0f751cb7ad31378d0c3dc33c0ab0ce9d2f858753
Abraham Sangha <abraham.sangha@cognizant.com>
2016-08-30 09:57:22 -0600

    Refer to Swifty service

--- app/controllers/feedbacks.js
+++ app/controllers/feedbacks.js
@@ -1,10 +1,11 @@
+var Swifty = require('../services/swifty');
 var FeedbacksController = {};
 
 FeedbacksController.create = function (req, res) {
   var pairPartner = req.body.text.split("@")[1].split(" ")[0];
   var pairInitiater = req.body.user_name;
   var feedback = req.body.text;
-  bot.postMessageToUser(pairPartner,  pairInitiater + " left you this feedback: " + feedback);
+  Swifty.postMessageToUser(pairPartner,  pairInitiater + " left you this feedback: " + feedback);
   res.send({
     status: "ok",
     text: "your feedback to " + pairPartner + "has been sent"
